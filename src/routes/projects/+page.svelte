c6caa189b3fca7834a296ef7df205ae7a9d0f9ad
ultrasive <github@aidanperry.net>
2023-10-18 18:52:31 -0500

    added technologies section to project page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -12,17 +12,15 @@
 			technologies: ['C', 'eBPF', 'XDP', 'Golang', 'SQL'],
 			githubLink: 'https://github.com/UltraSive/GoeBPF-XDP-Simple-Firewall'
 		},
-        {
+		{
 			name: 'Traffic Classifier NAT',
-			description:
-				'NAT performed on packets coming in and out of a machine based on a eBPF map.',
+			description: 'NAT performed on packets coming in and out of a machine based on a eBPF map.',
 			technologies: ['C', 'eBPF', 'TC', 'Golang', 'JSON'],
 			githubLink: 'https://github.com/UltraSive/GoeBPF-XDP-Simple-Firewall'
 		},
-        {
+		{
 			name: 'A2S Caching',
-			description:
-				'A2S_Info & A2S_Player Pakcet caching in XDP to reduce load on game servers.',
+			description: 'A2S_Info & A2S_Player Pakcet caching in XDP to reduce load on game servers.',
 			technologies: ['C', 'eBPF', 'XDP', 'Golang', 'SQL'],
 			githubLink: 'https://github.com/UltraSive/GoA2sCache'
 		},
@@ -33,13 +31,12 @@
 			technologies: ['Python', 'SvelteKit', 'JavaScript', 'APIs'],
 			githubLink: 'https://github.com/UltraSive/pterorust.com'
 		},
-        {
+		{
 			name: 'AidanPerry.net',
-			description:
-				'A personal website to display my projects and blog posts.',
-			technologies: [, 'SvelteKit', 'JavaScript', 'APIs', 'CMS'],
+			description: 'A personal website to display my projects and blog posts.',
+			technologies: ['SvelteKit', 'JavaScript', 'APIs', 'CMS'],
 			githubLink: 'https://github.com/UltraSive/aidanperry.net'
-		},
+		}
 	];
 
 	$: open = 'XDP Firewall';
@@ -64,6 +61,15 @@
 				{#if open == project.name}
 					<div class="p-4" transition:slide={{ duration: 1000, easing: elasticInOut }}>
 						{project.description}
+						<ul class="text-sm text-gray-600">
+							{#each project.technologies as tech}
+								<li
+									class="inline-block relative pr-8 last:pr-0 last-of-type:before:hidden before:absolute before:top-1/2 before:right-3 before:-translate-y-1/2 before:w-1 before:h-1 before:bg-gray-300 before:rounded-full dark:text-gray-400 dark:before:bg-gray-600"
+								>
+									{tech}
+								</li>
+							{/each}
+						</ul>
 					</div>
 				{/if}
 			</div>
